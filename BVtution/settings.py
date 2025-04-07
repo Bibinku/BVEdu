a0bd4bc6b81931f5cff9aa8205e91aa047ffb993
Bibinku <bibinku01@gmail.com>
2025-04-07 14:54:40 +0530

    Second commit

--- BVtution/settings.py
+++ BVtution/settings.py
@@ -23,9 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s@8^t@m8!!32%qgp&)$7oe)z^a_ioelek6ek8t+2rk3=3fg6z%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "bvedu.onrender.com",
+
+]
+
+
 
 
 # Application definition
@@ -41,8 +46,12 @@ INSTALLED_APPS = [
     'Frontend',
 ]
 
+
+CSRF_TRUSTED_ORIGINS = ['https://bvedu.onrender.com']
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,8 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is where static files will be collected
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+# Whitenoise settings
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
